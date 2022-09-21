824e6e68e8f3f7f0b0608ef7d59de1b7385db348
Christabelle143 <christabelleyouta143@gmail.com>
2022-09-21 19:38:05 +0100

    Add functionality for remove button

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 /* eslint-disable react/prop-types */
 /* eslint-disable  react/destructuring-assignment */
 function Book({ title, author, id }) {
+  const dispatch = useDispatch();
+  const deleteBook = (e) => {
+    e.preventDefault();
+    dispatch(removeBook(id));
+  };
   return (
     <>
       <div className="book">
         <h2 className="book-title">{title}</h2>
         <p className="book-author">{author}</p>
-        <button type="button" className="remove-btn" value={id}>Remove</button>
+        <button type="button" className="remove-btn" onClick={deleteBook} value={id}>Remove</button>
       </div>
     </>
   );
--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
 function AddBook() {
+  const bookList = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const [state, setState] = useState({ title: '', author: '' });
+
+  const read = (e) => {
+    const input = e.target;
+    setState({
+      ...state,
+      [input.name]: input.value,
+    });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    if (state.title && state.author) {
+      let maxID = 0;
+      for (let i = 0; i < bookList.books.length; i += 1) {
+        if (bookList.books[i].id > maxID) {
+          maxID = bookList.books[i].id;
+        }
+      }
+
+      dispatch(
+        addBook({
+          id: maxID + 1,
+          title: state.title,
+          author: state.author,
+        }),
+      );
+      setState({ title: '', author: '' });
+    }
+  };
   return (
     <div className="form-inputs">
       <h1>ADD A NEW BOOK</h1>
-      <form className="inputs">
-        <input type="text" id="title" placeholder="Bookk-title" />
-        <input type="text" id="author" placeholder="Book-author" />
+      <form className="inputs" onSubmit={submit}>
+        <input type="text" name="title" placeholder="Bookk-title" value={state.title} onChange={read} />
+        <input type="text" name="author" placeholder="Book-author" value={state.author} onChange={read} />
+        <button type="submit">Add</button>
       </form>
     </div>
   );
