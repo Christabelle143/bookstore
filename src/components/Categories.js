2d338462a17a9cb3557fe59730ae1417a7940a3d
Christabelle143 <christabelleyouta143@gmail.com>
2022-09-21 20:09:50 +0100

    dispatch action for category

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/Categories/Categories';
 
-function Categories() {
+// eslint-disable-next-line react/prop-types
+function Categories({ categories }) {
+  const dispatch = useDispatch();
+
+  const statusCheck = () => {
+    dispatch(checkStatus());
+  };
   return (
     <div className="check-btn">
-      <button type="button">Check status</button>
+      <button type="button" onClick={() => statusCheck()}>Check status</button>
+      <br />
+      { categories }
     </div>
   );
 }
+Categories.defaultProps = {
+  categories: [],
+};
 export default Categories;
