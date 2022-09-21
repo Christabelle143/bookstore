fa46407cfc6c3276ba65e92e260b6fa11789b7d0
Christabelle143 <christabelleyouta143@gmail.com>
2022-09-21 20:17:07 +0100

    modified index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Routes } from 'react-router-dom';
 import store from './redux/configureStore';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Routes>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Routes>
+  </React.StrictMode>,
 );
