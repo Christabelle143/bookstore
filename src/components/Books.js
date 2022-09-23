22154168fd70e79818cd09dd274b2a239c17222c
Christabelle143 <christabelleyouta143@gmail.com>
2022-09-23 13:10:22 +0100

    configure book reducer

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,17 +1,24 @@
 /* eslint-disable import/extensions */
 /* eslint-disable react/jsx-first-prop-new-line */
-import React from 'react';
 /* eslint-disable react/prop-types */
 /* eslint-disable  react/destructuring-assignment */
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
 import AddBook from './addBook';
+import { fetchBooks } from '../../redux/books/book';
 
-function Books(props) {
-  const { bookList } = props;
+function Books() {
+  const bookList = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [bookList, dispatch]);
+
   return (
     <div>
       {bookList.map((book) => (
-        <Book key={book.id} title={book.title} author={book.author} id={book.id} />
+        <Book key={book.item_id} title={book.title} author={book.author} id={book.item_id} />
       ))}
       <AddBook />
     </div>
