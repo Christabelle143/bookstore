47da52956ce8c6a73bf1b02929350b1b850e7cf7
Christabelle143 <christabelleyouta143@gmail.com>
2022-09-23 12:32:36 +0100

    fetch,delete and add book from API

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,42 +1,67 @@
 // Define action types for adding and removing a book.
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
 const ADD_BOOK = 'bookstore / books / ADD_BOOK';
 const REMOVE_BOOK = 'bookstore / books / REMOVE_BOOK';
-const initialState = [
-  {
-    id: 1,
-    title: 'THE HUNGER GAMES',
-    author: 'Suzanne Collins',
-  },
-  {
-    id: 2,
-    title: 'DUNE',
-    author: 'Frank Herbert',
-  },
-  {
-    id: 3,
-    title: 'Capital in the Twenty-first Century',
-    author: 'Suzanne Collins',
-  },
-];
+// const APP_ID = 'WLWRGsXNCux9kjlYUIID';
+const FETCH_BOOKS = 'bookstore/books/FETCH_BOOKS';
+
+const initialState = [];
 // Action Creators
-export const addBook = (book) => ({
-  type: ADD_BOOK,
-  payload: book,
+const addBook = createAsyncThunk(ADD_BOOK, async (book) => {
+  await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/WLWRGsXNCux9kjlYUIID/books/', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify(book),
+  });
+  return book;
+});
+const fetchBooks = createAsyncThunk(FETCH_BOOKS, async () => {
+  const response = await fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/WLWRGsXNCux9kjlYUIID/books/', {
+    method: 'GET',
+  });
+  const data = await response.json();
+  const bookIDs = Object.entries(data);
+  const books = [];
+  bookIDs.forEach(([key, book]) => {
+    const id = key;
+    const { title, author, category } = book[0];
+    books.push({
+      id, title, author, category,
+    });
+  });
+  return books;
 });
 
-export const removeBook = (index) => ({
-  type: REMOVE_BOOK,
-  payload: index,
+const removeBook = createAsyncThunk(REMOVE_BOOK, async (id) => {
+  await fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/WLWRGsXNCux9kjlYUIID/books/${id}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        item_id: id,
+      }),
+    });
+  return id;
 });
 // reducer
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
+    case FETCH_BOOKS:
+      return [...action.payload];
     case REMOVE_BOOK:
-      return [...state.filter((item) => item.id !== action.payload)];
+      return [...state.filter((book) => book.id !== action.payload)];
     default:
       return state;
   }
 };
 export default booksReducer;
+export { addBook, removeBook, fetchBooks };
