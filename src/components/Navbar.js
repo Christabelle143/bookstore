5301915ce612a0adfa477c39b7a340eb86a6cd12
Christabelle143 <christabelleyouta143@gmail.com>
2022-09-24 12:04:55 +0100

    install react-circular-progressbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div>
       <nav>
-        <h1>Bookstore CMS</h1>
+        <h1 className="first">Bookstore CMS</h1>
         <ul>
           <li>
             <Link to="/">Books</Link>
